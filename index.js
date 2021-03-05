b87d782cff1682671a165454a3882d44a335c93f
Szymon Szczepanowski <shymii@protonmail.com>
2021-03-05 20:37:34 +0100

    !frens

--- index.js
+++ index.js
@@ -139,6 +139,12 @@ bot.on('message', msg => {
                 .setImage(`attachment://${resp.pName}`)
                 msg.channel.send(embed)
             }
+            if(msgContent == 'frens'){
+                let embed = new Discord.MessageEmbed()
+                .setTitle('Frens to suchy pokarm do rozsądnego karmienia piesków. Jest hipoalergiczny, bezglutenowy i bezzbożowy, to znaczy, że idealny także dla czworonogów z wrażliwym żołądkiem. Karma Frens skomponowana została w oparciu o białko pochodzące z larw owadów. To sprawia, że proces produkcyjny jest bardziej etyczny i ekologiczny niż w przypadku karm mięsnych - oszczędza zwierzęta hodowlane i znaczące ilości wody. Karma produkowana i pakowana jest u naszych niemieckich sąsiadów według rygorystycznych standardów, z wyłącznie naturalnych i atestowanych składników.')
+                .setColor('#DD9ECD')
+                msg.channel.send(embed)
+            }
         }
     }
     
