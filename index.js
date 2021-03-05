37c96656ce013c9c6572796ea88d2f643f1db83f
Szymon Szczepanowski <shymii@protonmail.com>
2021-03-05 18:03:54 +0100

    fixed

--- index.js
+++ index.js
@@ -6,11 +6,12 @@ const TOKEN = process.env.TOKEN;
 
 let beautifulTimeout = null;
 let waterChannel = '680468339952189525';
+// test channel let waterChannel = '811917843771817994';
 
 
 let lastCiekawostka = null
-let ciekawostki = 
-[{content: 'czy wiedziałeś, że Ziemia jest jedyną planeta w układzie słonecznym której nazwa nie pochodzi od imienia boga', photo: null, pName: null}, 
+let ciekawostki = [
+{content: 'czy wiedziałeś, że Ziemia jest jedyną planeta w układzie słonecznym której nazwa nie pochodzi od imienia boga', photo: null, pName: null}, 
 {content: 'pięknie dziś wyglądasz:3', photo: null, pName: null}, 
 {content: 'czy wiedziałeś, że jedna sekunda to tysiąc milisekund?', photo: null, pName: null}, 
 {content: 'nie zapomnij o napiciu się wody!', photo: './images/woda.jpg', pName: 'woda.jpg'},
@@ -23,16 +24,16 @@ let ciekawostki =
 {content: 'gdy ci smutno, gdy ci źle, chodź do rio, przytul się', photo: './imgaes/rio.jpg', pName: 'rio.jpg'},
 {content: 'gapisz się jej na bebech?', photo: './images/edzia_bebech.jpg', pName: 'edzia_bebech.jpg'},
 {content: 'bóbr jest największym gryzoniem zamieszkującym europę', photo: './images/bobr.jpg', pName: 'bobr.jpg'},
-{content: null, photo: './images/mieciu.jpg', pName: 'mieciu.jpg'},
-{content: null, photo: './images/krulis.gif', pName: 'krulis.gif'},
-{content: null, photo: './images/krulisie_flower.jpg', pName: 'krulisie_flower.jpg'},
-{content: null, photo: './images/jajuwa.jpg', pName: 'jajuwa.jpg'},
+{content: '', photo: './images/mieciu.jpg', pName: 'mieciu.jpg'},
+{content: '', photo: './images/krulis.gif', pName: 'krulis.gif'},
+{content: '', photo: './images/krulisie_flower.jpg', pName: 'krulisie_flower.jpg'},
+{content: '', photo: './images/jajuwa.jpg', pName: 'jajuwa.jpg'},
 {content: 'hi u lil sweetie, smile more!' , photo: './images/lisek.jpg', pName: 'lisek.jpg'},
 {content: 'bóbr potrafi wstrzymać oddech nawet na kwadrans, co czyni go wyśmienitym nurkiem.', photo: null, pName: null},
 {content: 'dzięki wibrysom (włosom czuciowym) na pysku, boberki świetnie sobie radzą także w zanieczyszczonej, mętnej wodzie. :sweat_drops', photo: null, pName: null},
 {content: 'bóbr jest raczej małomówny – komunikaty głosowe wydaje bardzo rzadko. przed niebezpieczeństwem ostrzega innych członków stada poprzez głośne uderzanie ogonem o wodę.', photo: './images/boberki_woda.jpg', pName: 'boberki_woda.jpg'},
-{content: 'rzucilbys coś ten :point_right: :point_left:', photo: './images/rio_rzucaj.jpg', pName: 'rio_rzucaj.jpg'},
-{content: null, photo: './images/bunny_pie.jpg', pName: 'bunny_pie.jpg'},
+{content: 'rzuciłbys coś ten :point_right: :point_left:', photo: './images/rio_rzucaj.jpg', pName: 'rio_rzucaj.jpg'},
+{content: '', photo: './images/bunny_pie.jpg', pName: 'bunny_pie.jpg'},
 {content: 'miłego dnia tobie :dog:', photo: './images/milego_pjes.jpg', pName: 'milego_pjes.jpg'},
 {content: 'smacznej kawusi i jebać kapusi', photo: './images/kawunia.jpg', pName: 'kawunia.jpg'},
 {content: 'smacznej kawusi i jebać kapusi', photo: null, pName: null},
@@ -44,14 +45,24 @@ let ciekawostki =
 {content: 'smacznej herbatki dla całej rodzinki', photo: './images/rio_herbata.jpg', pName: 'rio_herbata.jpg'},
 {content: 'elo, napiles się dziś wody?', photo: './images/edzia_woda.jpg', pName: 'edzia_woda.jpg'},
 {content: 'jakiego masz windowsa', photo: './images/zeczak_windows.jpg', pName: 'zeczak_windows.jpg'},
-{content: null, photo: './images/maklowicz_pjes_plaza.jpg', pName: 'maklowicz_pjes_plaza.jpg'},
+{content: '', photo: './images/maklowicz_pjes_plaza.jpg', pName: 'maklowicz_pjes_plaza.jpg'},
 {content: 'halo dzwoni kuzyn', photo: './images/edzia_dzwoni.jpg', pName: 'edzia_dzwoni.jpg'},
-{content: null, photo: './images/krulis_hug.jpg', pName: 'krulis_hug.jpg'},
-{content: null, photo: './images/krulis_namiot.jpg', pName: 'krulis_namiot.jpg'},
+{content: '', photo: './images/krulis_hug.jpg', pName: 'krulis_hug.jpg'},
+{content: '', photo: './images/krulis_namiot.jpg', pName: 'krulis_namiot.jpg'},
 {content: 'dance monkey d(-__-)b', photo: './images/dance_monkey.gif', pName: 'dance_monkey.gif'},
 {content: '\***** \*** i konfederacje', photo: null, pName: null},
-{content: null, photo: './images/gorgeous_reminder.jpg', pName: 'gorgeous_reminder.jpg'},
-{content: null, photo: './images/mieciu_herbata.jpg', pName: 'mieciu_herbata.jpg'}
+{content: '', photo: './images/gorgeous_reminder.jpg', pName: 'gorgeous_reminder.jpg'},
+{content: '', photo: './images/whol1.jpg', pName: 'whol1.jpg'},
+{content: '', photo: './images/whol2.jpg', pName: 'whol2.jpg'},
+{content: '', photo: './images/whol3.jpg', pName: 'whol3.jpg'},
+{content: '', photo: './images/whol4.jpg', pName: 'whol4.jpg'},
+{content: '', photo: './images/whol5.jpg', pName: 'whol5.jpg'},
+{content: '', photo: './images/whol6.jpg', pName: 'whol6.jpg'},
+{content: '', photo: './images/whol7.jpg', pName: 'whol7.jpg'},
+{content: '', photo: './images/whol8.jpg', pName: 'whol8.jpg'},
+{content: 'szopy i wydry to epickie stworzenia', photo: './images/szop_wydra.jpg', pName: 'szop_wydra.jpg'},
+{content: 'weźże przykład z otta i poucz się trochę', photo: './images/otto_uczonko.jpg', pName: 'otto_uczonko.jpg'},
+{content: '', photo: './images/mieciu_herbata.jpg', pName: 'mieciu_herbata.jpg'}
 ];
 
 const losujCiekawostke = function(){
@@ -69,9 +80,11 @@ const urBeautiful = () => {
         let ciekawostka = losujCiekawostke();
         let embed = new Discord.MessageEmbed()
         .setTitle(ciekawostka.content)
-        .attachFiles([ciekawostka.photo])
-        .setImage(`attachment://${ciekawostka.pName}`)
         .setColor('#DD9ECD');
+        if(ciekawostka.photo){
+
+            embed.attachFiles([ciekawostka.photo]).setImage(`attachment://${ciekawostka.pName}`)
+        }
         bot.channels.cache.get(waterChannel).send(embed);
         urBeautiful();
     }, Math.floor(Math.random() * 25200000) + 36000000);
@@ -84,7 +97,7 @@ bot.login(TOKEN);
 
 bot.on('ready', () => {
     console.info(`Logged in as ${bot.user.tag}`)
-    bot.channels.cache.get(waterChannel).send('hej, jestem kruliś. robie wiele fajnych rzeczy jak np. przypominam o nawodnieniu organizmu albo daje dzienną dawkę ciekawostek. do zoba!');
+    //bot.channels.cache.get(waterChannel).send('hej, jestem kruliś. robie wiele fajnych rzeczy jak np. przypominam o nawodnieniu organizmu albo daje dzienną dawkę ciekawostek. do zoba!');
     urBeautiful();
 })
 
