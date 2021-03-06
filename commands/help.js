647a5c0a871c91201d6b9bb63d62cc61ed06ef97
Szymon Szczepanowski <shymii@protonmail.com>
2021-03-06 23:34:50 +0100

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -7,7 +7,7 @@ module.exports = {
         .setColor('#DD9ECD')
         .setTitle('Hej, jestem kruliś. Widzę, że potrzebujesz pomocy. Poniżej możesz zobaczyć jakie mam funkcje.')
         .addFields(
-            {name: '!banan :banana:', value: 'po wpisaniu tej komendy pokaże ci super krulika pozdro'},
+            {name: '!banan :banana:', value: 'po wpisaniu tej komendy pokaże ci super krulika'},
             {name: '!frens :nauseated_face:', value: 'zerzygam sie'},
             {name: '!help :question:', value: 'no pomoc i wogule'}
         )
