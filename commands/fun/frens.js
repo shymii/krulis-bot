5bdac172164965622bf4a9cde0749151a29866c0
Szymon Szczepanowski <shymii@protonmail.com>
2021-03-09 21:58:53 +0100

    Update frens.js

--- commands/fun/frens.js
+++ commands/fun/frens.js
@@ -2,8 +2,8 @@ const Discord = require('discord.js');
 
 const frens = [
     {content: 'Frens to suchy pokarm do rozsądnego karmienia piesków. Jest hipoalergiczny, bezglutenowy i bezzbożowy, to znaczy, że idealny także dla czworonogów z wrażliwym żołądkiem. Karma Frens skomponowana została w oparciu o białko pochodzące z larw owadów. To sprawia, że proces produkcyjny jest bardziej etyczny i ekologiczny niż w przypadku karm mięsnych - oszczędza zwierzęta hodowlane i znaczące ilości wody. Karma produkowana i pakowana jest u naszych niemieckich sąsiadów według rygorystycznych standardów, z wyłącznie naturalnych i atestowanych składników.', photo: null, pName: null},
-    {content: 'Frens to suchy pokarm do rozsądnego karmienia piesków. Jest hipoalergiczny, bezglutenowy i bezzbożowy, to znaczy, że idealny także dla czworonogów z wrażliwym żołądkiem. Karma Frens skomponowana została w oparciu o białko pochodzące z larw owadów. To sprawia, że proces produkcyjny jest bardziej etyczny i ekologiczny niż w przypadku karm mięsnych - oszczędza zwierzęta hodowlane i znaczące ilości wody. Karma produkowana i pakowana jest u naszych niemieckich sąsiadów według rygorystycznych standardów, z wyłącznie naturalnych i atestowanych składników.', photo: './imgaes/frens.gif', pName: 'frens.gif'},
-    {content: '', photo: './imgaes/frens_piona.png', pName: 'frens_piona.png'},
+    {content: 'Frens to suchy pokarm do rozsądnego karmienia piesków. Jest hipoalergiczny, bezglutenowy i bezzbożowy, to znaczy, że idealny także dla czworonogów z wrażliwym żołądkiem. Karma Frens skomponowana została w oparciu o białko pochodzące z larw owadów. To sprawia, że proces produkcyjny jest bardziej etyczny i ekologiczny niż w przypadku karm mięsnych - oszczędza zwierzęta hodowlane i znaczące ilości wody. Karma produkowana i pakowana jest u naszych niemieckich sąsiadów według rygorystycznych standardów, z wyłącznie naturalnych i atestowanych składników.', photo: './images/frens.gif', pName: 'frens.gif'},
+    {content: '', photo: './images/frens_piona.png', pName: 'frens_piona.png'},
 ]
 
 let lastFrens = null;
